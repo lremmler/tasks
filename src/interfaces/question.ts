175cc7b2fa56a569c55fce086c3b02cf682e656e
lremmler <lremmler@udel.edu>
2025-09-25 19:49:51 -0400

    Create question.ts

---
+++ src/interfaces/question.ts
@@ -0,0 +1,14 @@
+// interfaces/question.ts
+
+export type QuestionType = "multiple_choice_question" | "short_answer_question";
+
+export interface Question {
+    id: number;
+    name: string;
+    type: QuestionType;
+    body: string;
+    expected: string;
+    options: string[];
+    points: number;
+    published: boolean;
+}
