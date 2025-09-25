cb8a711015fe487a63b31a488459a8bd5b43dcca
lremmler <lremmler@udel.edu>
2025-09-25 17:50:25 -0400

    Update questions.json.ts

--- src/questions.json.ts
+++ src/questions.json.ts
@@ -1,2 +1,15 @@
 import questions from "./questions.json";
-export default questions;
+import { Question } from "./interfaces/question";
+
+const typedQuestions: Question[] = questions.map((q: Question) => ({
+    id: q.id,
+    name: q.name,
+    type: q.type,
+    body: q.body,
+    expected: q.expected,
+    options: [...q.options],
+    points: q.points,
+    published: q.published,
+}));
+
+export default typedQuestions;
