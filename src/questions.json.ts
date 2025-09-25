aaa299a98a2f1d029adeedd4fd0d0f1e01e8ef2a
lremmler <lremmler@udel.edu>
2025-09-25 17:34:37 -0400

    Update questions.json.ts

--- src/questions.json.ts
+++ src/questions.json.ts
@@ -1,24 +1,2 @@
-import { Question } from "./interfaces/question";
-
-export const questions: Question[] = [
-    {
-        id: 1,
-        name: "Addition Problem",
-        type: "short_answer_question",
-        body: "What is 2 + 2?",
-        expected: "4",
-        options: [],
-        points: 1,
-        published: false
-    },
-    {
-        id: 2,
-        name: "Color Question",
-        type: "multiple_choice_question",
-        body: "What is the color of the sky?",
-        expected: "Blue",
-        options: ["Blue", "Red", "Green", "Yellow"],
-        points: 2,
-        published: true
-    }
-];
+import questions from "./questions.json";
+export default questions;
