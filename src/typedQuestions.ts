f803e252f46f59b67b55a2b8411105543516cb56
lremmler <lremmler@udel.edu>
2025-09-25 17:07:53 -0400

    Update typedQuestions.ts

--- src/typedQuestions.ts
+++ src/typedQuestions.ts
@@ -1,12 +1,7 @@
-import raw from "./data/questions.json";
 import { Question } from "./interfaces/question";
+import rawQuestions from "./questions.json";
 
-interface QuestionData {
-    BLANK_QUESTIONS: Question[];
-    SIMPLE_QUESTIONS: Question[];
-}
+// Strongly typed array of questions
+const typedQuestions: Question[] = rawQuestions as Question[];
 
-const data = raw as unknown as QuestionData;
-
-export const BLANK_QUESTIONS: Question[] = data.BLANK_QUESTIONS;
-export const SIMPLE_QUESTIONS: Question[] = data.SIMPLE_QUESTIONS;
+export default typedQuestions;
