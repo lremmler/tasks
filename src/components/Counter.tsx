4f00bc457b3a5d0b769e168159f02ffd85f63e5b
lremmler <lremmler@udel.edu>
2025-09-25 20:32:51 -0400

    Create Counter.tsx

---
+++ src/components/Counter.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+// import { ChangeType } from "./components/ChangeType";
+// ...
+
+function App(): JSX.Element {
+    return (
+        <div className="App">
+            <header className="App-header">
+                UD CISC275 with React Hooks and TypeScript
+            </header>
+            <hr></hr>
+            <Counter></Counter>
+            <hr />
+            {/* <ChangeType></ChangeType> */}
