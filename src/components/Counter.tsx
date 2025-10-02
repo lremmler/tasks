6244e132420d89014a55e0c977c34e0b5e93ebd9
lremmler <lremmler@udel.edu>
2025-10-02 17:06:58 -0400

    Update Counter.tsx

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -4,10 +4,9 @@ export function Counter(): JSX.Element {
     const [value, setValue] = useState<number>(0);
     return (
         <div>
-            <h3>Counter Value: {value}</h3>
-            <button onClick={() => setValue(value + 1)}>Increment</button>
-            <button onClick={() => setValue(value - 1)}>Decrement</button>
-            <button onClick={() => setValue(0)}>Reset</button>
+            <button onClick={() => setValue(value + 1)}>Add One</button>
+            <div>Count: {value}</div>
+            <button onClick={() => setValue(value - 1)}>Subtract One</button>
         </div>
     );
 }
