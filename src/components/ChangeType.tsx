603415874f47e9a0f7ae4676dca2b2d522d63d7f
lremmler <lremmler@udel.edu>
2025-10-02 17:07:57 -0400

    Update ChangeType.tsx

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -2,15 +2,14 @@ import React, { useState } from "react";
 
 export function ChangeType(): JSX.Element {
     const [type, setType] = useState<string>("Short Answer");
-
-    const changeType = () => {
-        setType(type === "Short Answer" ? "Multiple Choice" : "Short Answer");
-    };
-
     return (
         <div>
-            <button onClick={changeType}>Change Type</button>
-            <div>Current Type: {type}</div>
+            <button onClick={() => setType(
+                type === "Multiple Choice" ? "Short Answer" : "Multiple Choice"
+            )}>
+                Change Type
+            </button>
+            {type === "Multiple Choice" ? "Multiple Choice" : "Short Answer"}
         </div>
     );
 }
